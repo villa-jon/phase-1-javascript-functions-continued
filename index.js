150345eb44117eb24dc21e401f12aafb615e847d
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-04-29 00:03:34 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!` 
+}
+saturdayFun('bathe my dog'); 
+
+function mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+mondayWork('work from home');
+
+function wrapAdjective(result='*') {
+       return function (emphatic = 'a hard worker'){
+            return (`You are ${result}${emphatic}${result}!`);
+    } 
+}
+wrapAdjective('||', 'a dedicated programmer')
